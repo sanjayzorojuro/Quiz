acb5d5c6b6c3a15d4e7a6b0b7077eeaaeb7d42bf
Sanjay <sanjayraops17@gmail.com>
2025-08-09 20:59:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -283,4 +283,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Database tables created successfully!")
-    app.run(debug=True)
+    app.run(debug=False)
